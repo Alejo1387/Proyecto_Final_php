c2f71747b65b837fb38cd80089e146df34e6ae38
Alejo1387 <trabajoalgoritmos02@gmail.com>
2025-07-19 15:48:00 -0500

    creacion de cuentas

---
+++ Back-End/PHP/Cuentas/cuentaCoor.php
@@ -0,0 +1,27 @@
+<?php
+
+    session_start();
+    if(!isset($_SESSION["usuario"])){
+        header("Location: ../Front-End/HTML/informacion.html");
+        exit();
+    }
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <script>
+        window.addEventListener("beforeunload", function (e) {
+            navigator.sendBeacon("../../../Back-End/PHP/logout.php");
+        });
+    </script>
+
+</body>
+</html>
