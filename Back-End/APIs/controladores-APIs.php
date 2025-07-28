0b6f89c5d78e403801eeede58cb0858245be96b1
Alejo1387 <trabajoalgoritmos02@gmail.com>
2025-07-27 20:12:52 -0500

    A controladores-APIs le agregue la funcion de crear coor, Al css le agregue estilos para ver mas hermoso el mensage de error al crear el coor, al crearCuenta html le modifique el script src y el boton de crear coor le agregue la funcion, al iniciarS le agregue la funcion de iniciar sesion y a funciones js lo modifique por completo convirtiendo todo en funciones

--- Back-End/APIs/controladores-APIs.php
+++ Back-End/APIs/controladores-APIs.php
@@ -7,6 +7,9 @@
 
     require_once "conexion.php";
 
+    // FUNCIONES
+
+    // Funcion de iniciar sesion
     function iniciarSesion($usuario, $clave) {
         global $pdo;
 
@@ -51,18 +54,324 @@
         exit;
     }
 
+    // Funcion de crear cuenta colegio
+    function crearCuentaColegio() {
+        global $pdo;
+
+        $nit = $_POST["nit"];
+        $nombre = $_POST["nombre"];
+        $direccion = $_POST["direccion"];
+        $telefono = $_POST["telefono"];
+        $email = $_POST["email"];
+        $pais = $_POST["pais"];
+        $departamento = $_POST["departamento"];
+        $ciudad = $_POST["ciudad"];
+        $descripcion = $_POST["descripcion"];
+        
+        // if (isset($_FILES["foto_perfil_colegio"]) && $_FILES["foto_perfil_colegio"]["error"] === 0) {
+        //     $foto = $_FILES["foto_perfil_colegio"]["name"];
+        // } else {
+        //     $foto = "";
+        // }
+
+        // $response = array($nit, $nombre, $direccion, $telefono, $email, $pais, $departamento, $ciudad, $descripcion, $foto);
+        // echo json_encode($response);
+
+        $nombreImagen = "defaultPerfilColegio.png";
+
+        if (isset($_FILES["foto_perfil_colegio"]) && $_FILES["foto_perfil_colegio"]["error"] === UPLOAD_ERR_OK) {
+            $archivoTmp = $_FILES["foto_perfil_colegio"]["tmp_name"];
+            $nombreOriginal = basename($_FILES["foto_perfil_colegio"]["name"]);
+
+            // Sanitizar nombre y evitar espacios raros
+            $nombreImagen = time() . "_" . preg_replace("/[^a-zA-Z0-9.]/", "_", $nombreOriginal);
+            $rutaDestino = __DIR__ . "/../uploads/" . $nombreImagen;
+
+            if (!move_uploaded_file($archivoTmp, $rutaDestino)) {
+                $response["errores"][] =  "❌ Error al subir la imagen. Asegúrate de que la carpeta 'uploads/' exista y tenga permisos.";
+                echo json_encode($response);
+                exit;
+            }
+        }
+
+        $sql = "INSERT INTO COLEGIOS (nit, nombre, direccion, telefono, email, pais, departamento, ciudad, descripcion, foto) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+        $stmt = $pdo->prepare($sql);
+
+        $sqlVerificarEmail = "
+        SELECT email FROM COORDINADORES WHERE email = ?
+        UNION
+        SELECT email FROM ESTUDIANTES WHERE email = ?
+        UNION
+        SELECT email FROM PROFESORES WHERE email = ?
+        UNION
+        SELECT email FROM COLEGIOS WHERE email = ?
+        ";
+        $stmt3 = $pdo->prepare($sqlVerificarEmail);
+
+        $sqlVerificarNumero = "
+        SELECT telefono FROM COORDINADORES WHERE telefono = ?
+        UNION
+        SELECT telefono FROM ESTUDIANTES WHERE telefono = ?
+        UNION
+        SELECT telefono FROM PROFESORES WHERE telefono = ?
+        UNION
+        SELECT telefono FROM COLEGIOS WHERE telefono = ?
+        ";
+        $stmt4 = $pdo->prepare($sqlVerificarNumero);
+
+        $sqlVerificarNit = "SELECT nit FROM COLEGIOS WHERE nit = ?";
+        $stmt5 = $pdo->prepare($sqlVerificarNit);
+
+        $response = [
+            "errores" => []
+        ];
+
+        try {
+            $stmt3->execute([$email, $email, $email, $email]);
+            $stmt4->execute([$telefono, $telefono, $telefono, $telefono]);
+            $stmt5->execute([$nit]);
+
+            if (mb_strlen($nit, 'UTF-8') > 20) {
+                $response["errores"][] = "El nit tiene una longitub mayor a lo permitido";
+            }
+            if ($stmt5->rowCount() > 0) {
+                $response["errores"][] = "Nit ya existente!";
+            }
+            if (mb_strlen($nombre, 'UTF-8') > 100) {
+                $response["errores"][] = "El nombre tiene una longitub mayor a lo permitido";
+            }
+            if (mb_strlen($direccion, 'UTF-8') > 150) {
+                $response["errores"][] = "La dirección tiene una longitub mayor a lo permitido";
+            }
+            if (mb_strlen($telefono, 'UTF-8') > 15) {
+                $response["errores"][] = "El telefono tiene una longitub mayor a lo permitido";
+            }
+            if ($stmt4->rowCount() > 0) {
+                $response["errores"][] = "Telefono ya existente!";
+            }
+            if (mb_strlen($email, 'UTF-8') > 100) {
+                $response["errores"][] = "El email tiene una longitub mayor a lo permitido";
+            }
+            if($stmt3->rowCount() > 0) {
+                $response["errores"][] = "Correo ya existente!";
+            }
+            if (mb_strlen($pais, 'UTF-8') > 50) {
+                $response["errores"][] = "El pais tiene una longitub mayor a lo permitido";
+            }
+            if (mb_strlen($departamento, 'UTF-8') > 50) {
+                $response["errores"][] = "El departamento tiene una longitub mayor a lo permitido";
+            }
+            if (mb_strlen($ciudad, 'UTF-8') > 50) {
+                $response["errores"][] = "La ciudad tiene una longitub mayor a lo permitido";
+            }
+
+            if (!empty($response["errores"])) {
+                echo json_encode($response);
+                exit;
+            }
+
+            $stmt->execute([$nit, $nombre, $direccion, $telefono, $email, $pais, $departamento, $ciudad, $descripcion, $nombreImagen]);
+
+            $idColegio = $pdo->lastInsertId();
+
+            $response["idColegio"] = $idColegio;
+            echo json_encode($response);
+            exit;
+        } catch (Exception $e) {
+            $response["success"] = false;
+            $response["message"] = "❌ Error al insertar en la base de datos: " . $e->getMessage();
+            echo json_encode($response);
+        }
+        $response["errores"] = [];
+
+        exit;
+    }
+
+    // Funcion para crear un coor
+    function formularioCrearCuentaCoor() {
+        global $pdo;
+
+        $id_col =  $_POST["id_col"];
+        $tipo_documento = $_POST["tipo_documento"];
+        $numero_documento = $_POST["numero_documento"];
+        $nombres = $_POST["nombres"];
+        $apellidos = $_POST["apellidos"];
+        $fecha_nacimiento = $_POST["fecha_nacimiento"];
+        $generoo = $_POST["generoo"];
+        $email = $_POST["email"];
+        $telefono = $_POST["telefono"];
+        $direccion = $_POST["direccion"];
+        $descripcion = $_POST["descripcion"];
+        $usuario = $_POST["usuario"];
+        $clave = $_POST["clave"];
+
+        // if (isset($_FILES["foto_coor"]) && $_FILES["foto_coor"]["error"] === 0) {
+        //     $foto = $_FILES["foto_coor"]["name"];
+        // } else {
+        //     $foto = "";
+        // }
+
+        $nombreImagen = "defaultPerfilColegio.png";
+
+        if (isset($_FILES["foto_coor"]) && $_FILES["foto_coor"]["error"] === UPLOAD_ERR_OK) {
+            $archivoTmp = $_FILES["foto_coor"]["tmp_name"];
+            $nombreOriginal = basename($_FILES["foto_coor"]["name"]);
+
+            $nombreImagen = time() . "_" . preg_replace("/[^a-zA-Z0-9.]/", "_", $nombreOriginal);
+            $rutaDestino = __DIR__ . "/../uploads/" . $nombreImagen;
+
+            if (!move_uploaded_file($archivoTmp, $rutaDestino)) {
+                $response["errores"][] =  "❌ Error al subir la imagen del coordinador.";
+                echo json_encode($response);
+                exit;
+            }
+        }
+
+        $sql = "INSERT INTO COORDINADORES(colegio_id, tipo_documento, numero_documento, nombres, apellidos, fecha_nacimiento, genero, email, telefono, direccion, usuario, clave, descripcion, foto) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+        $stmt = $pdo->prepare($sql);
+
+        $sqlVerificarEmail = "
+        SELECT email FROM COORDINADORES WHERE email = ?
+        UNION
+        SELECT email FROM ESTUDIANTES WHERE email = ?
+        UNION
+        SELECT email FROM PROFESORES WHERE email = ?
+        UNION
+        SELECT email FROM COLEGIOS WHERE email = ?
+        ";
+        $stmt3 = $pdo->prepare($sqlVerificarEmail);
+
+        $sqlVerificarNumero = "
+        SELECT telefono FROM COORDINADORES WHERE telefono = ?
+        UNION
+        SELECT telefono FROM ESTUDIANTES WHERE telefono = ?
+        UNION
+        SELECT telefono FROM PROFESORES WHERE telefono = ?
+        UNION
+        SELECT email FROM COLEGIOS WHERE telefono = ?
+        ";
+        $stmt4 = $pdo->prepare($sqlVerificarNumero);
+
+        $sqlVerificarUsuario = "
+        SELECT usuario FROM COORDINADORES WHERE usuario = ?
+        UNION
+        SELECT usuario FROM ESTUDIANTES WHERE usuario = ?
+        UNION
+        SELECT usuario FROM PROFESORES WHERE usuario = ?
+        ";
+        $stmt5 = $pdo->prepare($sqlVerificarUsuario);
+
+        $sqlVerificarNumeroDocumento = "SELECT numero_documento FROM COORDINADORES WHERE numero_documento = ?";
+        $stmt6 = $pdo->prepare($sqlVerificarNumeroDocumento);
+
+        $response = [
+            "errores" => []
+        ];
+
+        try {
+            $stmt3->execute([$email, $email, $email, $email]);
+            $stmt4->execute([$telefono, $telefono, $telefono, $telefono]);
+            $stmt5->execute([$usuario ,$usuario ,$usuario]);
+            $stmt6->execute([$numero_documento]);
+
+            if (mb_strlen($numero_documento, 'UTF-8') > 20) {
+                $response["errores"][] = "El numero de documento tiene una longitub mayor a lo permitido";
+            }
+            if ($stmt6->rowCount() > 0) {
+                $response["errores"][] = "Numero de documento ya existente!";
+            }
+            if (mb_strlen($nombres, 'UTF-8') > 60) {
+                $response["errores"][] = "El nombre tiene una longitub mayor a lo permitido";
+            }
+            if (mb_strlen($apellidos, 'UTF-8') > 60) {
+                $response["errores"][] = "El apellido tiene una longitub mayor a lo permitido";
+            }
+            if (mb_strlen($email, 'UTF-8') > 100) {
+                $response["errores"][] = "El email tiene una longitub mayor a lo permitido";
+            }
+            if ($stmt3->rowCount() > 0) {
+                $response["errores"][] = "Correo ya existente!";
+            }
+            if (mb_strlen($telefono, 'UTF-8') > 15) {
+                $response["errores"][] = "El telefono tiene una longitub mayor a lo permitido";
+            }
+            if ($stmt4->rowCount() > 0) {
+                $response["errores"][] = "Telefono ya existente!";
+            }
+            if (mb_strlen($direccion, 'UTF-8') > 150) {
+                $response["errores"][] = "La dirección tiene una longitub mayor a lo permitido";
+            }
+            if (mb_strlen($usuario, 'UTF-8') > 50) {
+                $response["errores"][] = "El usuario tiene una longitub mayor a lo permitido";
+            }
+            if ($stmt5->rowCount() > 0) {
+                $response["errores"][] = "Usuario ya existente!";
+            }
+            if (mb_strlen($clave, 'UTF-8') > 255) {
+                $response["errores"][] = "La clave tiene una longitub mayor a lo permitido";
+            }
+
+            if (!empty($response["errores"])) {
+                echo json_encode($response);
+                exit;
+            }
+
+            $stmt->execute([$id_col, $tipo_documento, $numero_documento, $nombres, $apellidos, $fecha_nacimiento, $generoo, $email, $telefono, $direccion, $usuario, $clave, $descripcion, $nombreImagen]);
+
+            echo json_encode($response);
+            exit;
+        } catch (Exception $e) {
+            $response["success"] = false;
+            $response["message"] = "❌ Error al insertar en la base de datos: " . $e->getMessage();
+            echo json_encode($response);
+        }
+        $response["errores"] = [];
+    }
+
+
+    // CONDICIONALES PARA SABER LA ACCION Y LA FUNCION
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $accion = $_POST["accion"];
 
+        // Condicional para saber la accion
         switch ($accion) {
+            // Casos GET
             case 'GET':
                 $funcion = $_POST["funcion"];
+
+                // Condicional para saber la funcion
                 switch ($funcion) {
+                    
+                    // Funcion para iniciar sesion
                     case 'iniciarSesion':
                         $usuario = $_POST["usuario"];
                         $clave = $_POST["clave"];
                         iniciarSesion($usuario, $clave);
                         break;
+                    
+                    // Por si no llega la funcion
+                    default:
+                        echo json_encode("Error");
+                }
+                break;
+
+            // Casos INSERT
+            case 'INSERT':
+                $funcion = $_POST["funcion"];
+
+                // Condicional para saber la funcion
+                switch ($funcion) {
+
+                    // Funcion para insertar un nuevo colegio
+                    case 'crearCuentaColegio':
+                        crearCuentaColegio();
+                        break;
+                    
+                    // funcion para insertar un nuevo coordinador
+                    case 'formularioCrearCuentaCoor':
+                        formularioCrearCuentaCoor();
+                        break;
+                    
                     // Por si no llega la funcion
                     default:
                         echo json_encode("Error");
--- Front-End/Js/funciones.js
+++ Front-End/Js/funciones.js
@@ -1,38 +1,193 @@
-const formularioIniciarS = document.getElementById('form_iniciarS');
+// FUNCIONES
 
-formularioIniciarS.addEventListener('submit', function(event) {
-    event.preventDefault();
+// Funcion de iniciar sesion
+function formularioIniciarS() {
+    const formularioIniciarS = document.getElementById('form_iniciarS');
 
-    const usuario = document.getElementById('usuario').value;
-    const clave = document.getElementById('clave').value;
+    if(!formularioIniciarS) return;
 
-    const formData = new FormData();
-    formData.append('accion', 'GET');
-    formData.append('funcion', 'iniciarSesion');
-    formData.append('usuario', usuario);
-    formData.append('clave', clave);
+    formularioIniciarS.addEventListener('submit', function(event) {
+        event.preventDefault();
 
-    fetch ('../../Back-End/APIs/controladores-APIs.php', {
-        method: "POST",
-        body: formData
-    })
-    .then(res => res.json())
-    .then(data => {
-        switch (data.success) {
-            case 'Coor':
-                window.location.href = '../../Back-End/PHP/Cuentas/cuentaCoor.php';
-                break;
-            case 'Prof':
-                window.location.href = '../../Back-End/PHP/Cuentas/cuentaProf.php';
-                break;
-            case 'Estu':
-                window.location.href = '../../Back-End/PHP/Cuentas/cuentaEstu.php';
-                break;
-            case 'noLogin':
-                document.getElementById('error').textContent = "Usuario o clave incorrectos.";
-                break;
-            default:
-                alert('⚠️ Error inesperado en la respuesta');
-        }
+        const usuario = document.getElementById('usuario').value;
+        const clave = document.getElementById('clave').value;
+
+        const formData = new FormData();
+        formData.append('accion', 'GET');
+        formData.append('funcion', 'iniciarSesion');
+        formData.append('usuario', usuario);
+        formData.append('clave', clave);
+
+        fetch ('../../Back-End/APIs/controladores-APIs.php', {
+            method: "POST",
+            body: formData
+        })
+        .then(res => res.json())
+        .then(data => {
+            switch (data.success) {
+                case 'Coor':
+                    window.location.href = '../../Back-End/PHP/Cuentas/cuentaCoor.php';
+                    break;
+                case 'Prof':
+                    window.location.href = '../../Back-End/PHP/Cuentas/cuentaProf.php';
+                    break;
+                case 'Estu':
+                    window.location.href = '../../Back-End/PHP/Cuentas/cuentaEstu.php';
+                    break;
+                case 'noLogin':
+                    document.getElementById('error').textContent = "Usuario o clave incorrectos.";
+                    break;
+                default:
+                    alert('⚠️ Error inesperado en la respuesta');
+            }
+        });
     });
-});
+}
+
+// Funcion de formularioCrearCuentaColegio
+function formularioCrearCuentaColegio() {
+    const formularioCrearCuentaColegio = document.getElementById('formulario_crearColegio');
+
+    if(!formularioCrearCuentaColegio) return;
+
+    formularioCrearCuentaColegio.addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        const nit = document.getElementById('nit_Colegio').value;
+        const nombre = document.getElementById('nombre_Colegio').value;
+        const direccion = document.getElementById('direccion_Colegio').value;
+        const telefono = document.getElementById('telefono_Colegio').value;
+        const email = document.getElementById('email_Colegio').value;
+        const pais = document.getElementById('pais_Colegio').value;
+        const departamento = document.getElementById('departamento_Colegio').value;
+        const ciudad = document.getElementById('ciudad_Colegio').value;
+        const descripcion = document.getElementById('descripcion_Colegio').value;
+        const foto_perfil_colegio = document.getElementById('foto_Colegio').files[0];
+
+        const formData = new FormData();
+        formData.append('accion', 'INSERT');
+        formData.append('funcion', 'crearCuentaColegio');
+        formData.append('nit', nit);
+        formData.append('nombre', nombre);
+        formData.append('direccion', direccion);
+        formData.append('telefono', telefono);
+        formData.append('email', email);
+        formData.append('pais', pais);
+        formData.append('departamento', departamento);
+        formData.append('ciudad', ciudad);
+        formData.append('descripcion', descripcion);
+        formData.append('foto_perfil_colegio', foto_perfil_colegio);
+
+        fetch('../../Back-End/APIs/controladores-APIs.php', {
+            method: "POST",
+            body: formData
+        })
+        .then(res => res.json())
+        .then(resultado => {
+            const errores = resultado.errores;
+
+            if (Array.isArray(errores) && errores.length > 0) {
+                document.getElementById('mensaje_Col').textContent = "❌ " + errores[0];
+                document.getElementById('mensaje_Col2').textContent = "❌ " + errores[0];
+
+                document.getElementById('mensaje_Col').style.background = '#39ff14';
+                document.getElementById('mensaje_Col2').style.background = '#39ff14';
+            } else {
+                const id_colegio = resultado.idColegio;
+
+                document.getElementById('mensaje_Col').style.background = "none";
+                document.getElementById('mensaje_Col2').style.background = "none";
+                document.getElementById('mensaje_Col').textContent = "";
+                document.getElementById('mensaje_Col2').textContent = "";
+
+                formularioCrearCuentaColegio.reset();
+
+                // Si en PHP guardaste en localStorage que debe ir a página 2:
+                localStorage.setItem('seccionActualCrearC', 'pagina2');
+                localStorage.setItem('idColegioInsertado', id_colegio);
+                window.location.href = window.location.href;
+            }
+        })
+        .catch(err => {
+            console.error("Error al enviar el formulario:", err);
+            alert("❌ Error inesperado al enviar el formulario.");
+        });
+    });
+}
+
+// Funcion de formularioCrearCuentaCoor
+function formularioCrearCuentaCoor() {
+    const formularioCrearCuentaCoor = document.getElementById('formulario_crearCuentaCoor_pagina2');
+
+    if(!formularioCrearCuentaCoor) return;
+
+    formularioCrearCuentaCoor.addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        const id_col = document.getElementById('id_col').value;
+        const tipo_documento = document.getElementById('tipo_documento').value;
+        const numero_documento = document.getElementById('numero_documento').value;
+        const nombres = document.getElementById('nombres_coor').value;
+        const apellidos = document.getElementById('apellidos').value;
+        const fecha_nacimiento = document.getElementById('fecha_nacimiento').value;
+        const generoo = document.getElementById('generoo').value;
+        const email = document.getElementById('email_coor').value;
+        const telefono = document.getElementById('telefono_coor').value;
+        const direccion = document.getElementById('direccion_coor').value;
+        const descripcion = document.getElementById('descripcion_coor').value;
+        const usuario = document.getElementById('usuario').value;
+        const clave = document.getElementById('clave').value;
+        const foto_coor = document.getElementById('foto_coor').files[0];
+
+        const formData = new FormData();
+        formData.append('accion', 'INSERT');
+        formData.append('funcion', '../../Back-End/APIs/controladores-APIs.php');
+        formData.append('id_col', id_col);
+        formData.append('tipo_documento', tipo_documento);
+        formData.append('numero_documento', numero_documento);
+        formData.append('nombres', nombres);
+        formData.append('apellidos', apellidos);
+        formData.append('fecha_nacimiento', fecha_nacimiento);
+        formData.append('generoo', generoo);
+        formData.append('email', email);
+        formData.append('telefono', telefono);
+        formData.append('direccion', direccion);
+        formData.append('descripcion', descripcion);
+        formData.append('usuario', usuario);
+        formData.append('clave', clave);
+        formData.append('foto_coor', foto_coor);
+
+        fetch('../../Back-End/APIs/crearCuentaCoor.php', {
+            method: "POST",
+            body: formData
+        })
+        .then(res => res.json())
+        .then(resultado => {
+            const errores = resultado.errores;
+
+            if (Array.isArray(errores) && errores.length > 0) {
+                document.getElementById('mensaje_coor').textContent = "❌ " + errores[0];
+                document.getElementById('mensaje_coor2').textContent = "❌ " + errores[0];
+
+                document.getElementById('mensaje_coor').style.background = '#39ff14';
+                document.getElementById('mensaje_coor').style.background = '#39ff14';
+            } else {
+                document.getElementById('mensaje_coor').style.background = "none";
+                document.getElementById('mensaje_coor2').style.background = "none";
+                document.getElementById('mensaje_coor').textContent = "";
+                document.getElementById('mensaje_coor2').textContent = "";
+
+                formularioCrearCuentaCoor.reset();
+
+                // Si en PHP guardaste en localStorage que debe ir a página 2:
+                localStorage.setItem('seccionActualCrearC', 'pagina1');
+                localStorage.setItem('idColegioInsertado', 0);
+                window.location.href = "../HTML/iniciarS.html";
+            }
+        })
+        .catch(error => {
+            console.error("❌ Error al enviar el formulario del coordinador:", error);
+            alert("❌ Error inesperado. Revisa consola (F12).");
+        });
+    });
+}
